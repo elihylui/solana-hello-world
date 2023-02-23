6f1cae53308a2f7dac268affb13d39d137bde319
Elizabeth <elihylui@gmail.com>
2023-02-23 17:45:08 +0000

    add update message

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useAnchorWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import useIsMounted from "./api/utils/useIsMounted";
 import createMessage from "./api/createMessage";
+import updateMessage from "./api/updateMessage";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
   const [messageAuthor, setMessageAuthor] = useState("");
   const [messageTime, setMessageTime] = useState(0);
   const [inputtedMessage, setInputtedMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const wallet = useAnchorWallet();
   const mounted = useIsMounted();
@@ -38,34 +40,47 @@ export default function Home() {
               className={styles.message_button}
               disabled={!inputtedMessage}
               onClick={async () => {
-                const message = await createMessage(
-                  inputtedMessage,
-                  wallet,
-                  messageAccount
-                );
-                if (message) {
-                  setMessage(message.content.toString());
-                  setMessageAuthor(message.author.toString());
-                  setMessageTime(message.timestamp.toNumber() * 1000);
+                setLoading(true);
+                const deployedMessage = message
+                  ? await updateMessage(inputtedMessage, wallet, messageAccount)
+                  : await createMessage(
+                      inputtedMessage,
+                      wallet,
+                      messageAccount
+                    );
+
+                if (deployedMessage) {
+                  setMessage(deployedMessage.content.toString());
+                  setMessageAuthor(deployedMessage.author.toString());
+                  setMessageTime(deployedMessage.timestamp.toNumber() * 1000);
                   setInputtedMessage("");
                 }
+                setLoading(false);
               }}
             >
-              Create a Message!
+              {message ? "Update the Message!" : "Create a Message!"}
             </button>
           </div>
         )}
 
-        {wallet && message && (
-          <div className={styles.card}>
-            <h2>Current Message: {message}</h2>
-            <h2>
-              Message Author: {messageAuthor.substring(0, 4)}
-              ...
-              {messageAuthor.slice(-4)}
-            </h2>
-            <h2>Time Published: {new Date(messageTime).toLocaleString()}</h2>
+        {loading ? (
+          <div className={styles.loader_bar}>
+            <h2> Loading</h2>
+            <div className={styles.loader} />
           </div>
+        ) : (
+          wallet &&
+          message && (
+            <div className={styles.card}>
+              <h2>Current Message: {message}</h2>
+              <h2>
+                Message Author: {messageAuthor.substring(0, 4)}
+                ...
+                {messageAuthor.slice(-4)}
+              </h2>
+              <h2>Time Published: {new Date(messageTime).toLocaleString()}</h2>
+            </div>
+          )
         )}
       </div>
     </div>
